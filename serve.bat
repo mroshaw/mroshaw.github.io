34ecf8ec48c0501c8ea905978430b1344f93fc0b
mroshaw <oli@mroshaw.co.uk>
2026-05-25 14:19:29 +0100

    Added helper scripts to build, server and navigate to the docs site

---
+++ serve.bat
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy Bypass -File "%~dp0serve.ps1"
---
+++ serve.ps1
@@ -0,0 +1,8 @@
+# Launch browser in background after a short delay
+Start-Job -ScriptBlock {
+    Start-Sleep -Seconds 3
+    Start-Process "http://127.0.0.1:8000/"
+} | Out-Null
+
+# Run MkDocs in the foreground - Ctrl+C will stop it as normal
+python -m mkdocs serve
