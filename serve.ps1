ea23c1f3a161ee6aba21f3f3ce0409a57014b99a
mroshaw <oli@mroshaw.co.uk>
2026-05-27 11:39:30 +0100

    QoL navigation config enabled

--- serve.ps1
+++ serve.ps1
@@ -1,7 +1,7 @@
 # Launch browser in background after a short delay
 Start-Job -ScriptBlock {
     Start-Sleep -Seconds 3
-    Start-Process "http://127.0.0.1:8000/"
+    Start-Process "http://localhost:8000/"
 } | Out-Null
 
 # Run Zensical in the foreground - Ctrl+C will stop it as normal
